675ca462203da7bcb3185f37f8f4dcd3dcc652d1
sharky564 <sharkesa@gmail.com>
2023-12-12 10:21:03 +1100

    Implemented Part 1 and Part 2 in Python and C++.

--- advent_of_code_2023_q2.cpp
+++ advent_of_code_2023_q2.cpp
@@ -5,7 +5,47 @@
 #include <vector>
 #include <algorithm>
 
-int main() {
+
+void part1() {
+    std::ifstream file("input.txt");
+    std::string line;
+    int sum = 0;
+    while (std::getline(file, line)) {
+        for (int i = 0; i < line.length(); i++) {
+            if (line[i] == ':' || line[i] == ',' || line[i] == ';') {
+                line[i] = ' ';
+            }
+        }
+        // split the line into words
+        std::vector<std::string> words;
+        std::stringstream ss(line);
+        std::string word;
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        int id = std::stoi(words[1]);
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+        for (int i = 2; i < words.size(); i++) {
+            if (words[i] == "red") {
+                red = std::max(red, std::stoi(words[i-1]));
+            } else if (words[i] == "green") {
+                green = std::max(green, std::stoi(words[i-1]));
+            } else if (words[i] == "blue") {
+                blue = std::max(blue, std::stoi(words[i-1]));
+            }
+        }
+        if ((red <= 12) && (green <= 13) && (blue <= 14)) {
+            sum += id;    
+        }
+    }
+    std::cout << sum << std::endl;
+
+}
+
+
+void part2() {
     std::ifstream file("input.txt");
     std::string line;
     int sum = 0;
@@ -38,5 +78,10 @@ int main() {
         sum += red * green * blue;
     }
     std::cout << sum << std::endl;
+}
+
+int main() {
+    part1();
+    part2();
     return 0;
 }
--- advent_of_code_2023_q2.py
+++ advent_of_code_2023_q2.py
@@ -1,22 +1,53 @@
-f = open("Advent of Code/input.txt", 'r')
+f = open("input.txt", 'r')
 out = f.read().split('\n')
-total = 0
-for line in out:
-    game, draws = line.split(':')
-    id = int(game.split()[1])
-    draws = draws.split(';')
-    draws = [draw.split(',') for draw in draws]
-    draws = [[tup.split() for tup in draw] for draw in draws]
-    red = 0
-    green = 0
-    blue = 0
-    for draw in draws:
-        for tup in draw:
-            if tup[1] == 'red':
-                red = max(red, int(tup[0]))
-            elif tup[1] == 'green':
-                green = max(green, int(tup[0]))
-            elif tup[1] == 'blue':
-                blue = max(blue, int(tup[0]))
-    total += red * green * blue
-print(total)
+
+
+def part1():
+    total = 0
+    for line in out:
+        game, draws = line.split(':')
+        id = int(game.split()[1])
+        draws = draws.split(';')
+        draws = [draw.split(',') for draw in draws]
+        draws = [[tup.split() for tup in draw] for draw in draws]
+        red = 0
+        green = 0
+        blue = 0
+        for draw in draws:
+            for tup in draw:
+                if tup[1] == 'red':
+                    red = max(red, int(tup[0]))
+                elif tup[1] == 'green':
+                    green = max(green, int(tup[0]))
+                elif tup[1] == 'blue':
+                    blue = max(blue, int(tup[0]))
+        if red <= 12 and green <= 13 and blue <= 14:
+            total += id
+    print(total)
+
+
+def part2():
+    total = 0
+    for line in out:
+        game, draws = line.split(':')
+        id = int(game.split()[1])
+        draws = draws.split(';')
+        draws = [draw.split(',') for draw in draws]
+        draws = [[tup.split() for tup in draw] for draw in draws]
+        red = 0
+        green = 0
+        blue = 0
+        for draw in draws:
+            for tup in draw:
+                if tup[1] == 'red':
+                    red = max(red, int(tup[0]))
+                elif tup[1] == 'green':
+                    green = max(green, int(tup[0]))
+                elif tup[1] == 'blue':
+                    blue = max(blue, int(tup[0]))
+        total += red * green * blue
+    print(total)
+
+
+part1()
+part2()
