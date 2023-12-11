0be2eebed3d9368ca598d57a8f1b8c6b3dd8d1b4
sharky564 <sharkesa@gmail.com>
2023-12-12 10:12:13 +1100

    Now have both parts of Day 1 implemented in Python and C++.

--- advent_of_code_2023_q1.cpp
+++ advent_of_code_2023_q1.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
-int main() {
+void part1() {
     std::ifstream file("input.txt");
     std::string line;
     int sum = 0;
@@ -26,5 +27,62 @@ int main() {
         sum += 10 * first + last;
     }
     std::cout << sum << std::endl;
+}
+
+void part2() {
+    std::ifstream file("input.txt");
+    std::string line;
+    int sum = 0;
+    std::vector<std::string> numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    while (std::getline(file, line)) {
+        int first = 0;
+        int last = 0;
+        int i = 0;
+        while (i < line.length()) {
+            if (isdigit(line[i])) {
+                first = line[i] - '0';
+                break;
+            }
+            else {
+                for (int j = 0; j < numbers.size(); j++) {
+                    if (line.substr(i, numbers[j].length()) == numbers[j]) {
+                        first = j + 1;
+                        i = line.length();
+                        break;
+                    }
+                }
+            }
+            i++;
+        }
+        if (first == 0) {
+            continue;
+        }
+
+        i = line.length() - 1;
+        while (i >= 0) {
+            if (isdigit(line[i])) {
+                last = line[i] - '0';
+                break;
+            }
+            else {
+                for (int j = 0; j < numbers.size(); j++) {
+                    if (line.substr(i, numbers[j].length()) == numbers[j]) {
+                        last = j + 1;
+                        i = -1;
+                        break;
+                    }
+                }
+            }
+            i--;
+        }
+        sum += 10 * first + last;
+    }
+    std::cout << sum << std::endl;
+}
+
+
+int main() {
+    part1();
+    part2();
     return 0;
 }
--- advent_of_code_2023_q1.py
+++ advent_of_code_2023_q1.py
@@ -1,47 +1,81 @@
 f = open("input.txt", 'r')
 out = f.read().split('\n')
-total = 0
-nums = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
-# out = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
-for line in out:
-    # find the first instance of a digit or a value in nums
-    found_first_digit = False
-    first_digit = 0
-    found_second_digit = False
-    second_digit = 0
-    i = 0
-    while i < len(line) and not found_first_digit:
-        if line[i].isdigit():
-            first_digit = int(line[i])
-            found_first_digit = True
-        else:
-            for key, val in nums.items():
-                if line[i:i+len(val)] == val:
-                    first_digit = key
-                    found_first_digit = True
-                    break
-        i += 1
-    
-    if not found_first_digit:
-        continue
 
-    # find the last instance of a digit or a value in nums
-    i = len(line) - 1
-    while i >= 0 and not found_second_digit:
-        if line[i].isdigit():
-            second_digit = int(line[i])
-            found_second_digit = True
-        else:
-            for key, val in nums.items():
-                if line[i-len(val)+1:i+1] == val:
-                    second_digit = key
-                    found_second_digit = True
-                    break
-        i -= 1
-    if not found_second_digit:
-        continue
+def part1():
+    total = 0
+    for line in out:
+        found_first_digit = False
+        first_digit = 0
+        i = 0
+        while i < len(line) and not found_first_digit:
+            if line[i].isdigit():
+                first_digit = int(line[i])
+                found_first_digit = True
+            i += 1
+        
+        if not found_first_digit:
+            continue
+        
+        found_second_digit = False
+        second_digit = 0
+        i = len(line) - 1
+        while i >= 0 and not found_second_digit:
+            if line[i].isdigit():
+                second_digit = int(line[i])
+                found_second_digit = True
+            i -= 1
+        if not found_second_digit:
+            continue
 
-    if found_first_digit and found_second_digit:
-        total += 10 * first_digit + second_digit
-        print(line, total, first_digit, second_digit)
-print(total)
+        if found_first_digit and found_second_digit:
+            total += 10 * first_digit + second_digit
+
+    print(total)
+
+def part2():
+    total = 0
+    nums = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+    for line in out:
+        found_first_digit = False
+        first_digit = 0
+        i = 0
+        while i < len(line) and not found_first_digit:
+            if line[i].isdigit():
+                first_digit = int(line[i])
+                found_first_digit = True
+            else:
+                for key, val in nums.items():
+                    if line[i:i+len(val)] == val:
+                        first_digit = key
+                        found_first_digit = True
+                        break
+            i += 1
+        
+        if not found_first_digit:
+            continue
+        
+        found_second_digit = False
+        second_digit = 0
+        i = len(line) - 1
+        while i >= 0 and not found_second_digit:
+            if line[i].isdigit():
+                second_digit = int(line[i])
+                found_second_digit = True
+            else:
+                for key, val in nums.items():
+                    if line[i-len(val)+1:i+1] == val:
+                        second_digit = key
+                        found_second_digit = True
+                        break
+            i -= 1
+        if not found_second_digit:
+            continue
+
+        if found_first_digit and found_second_digit:
+            total += 10 * first_digit + second_digit
+
+    print(total)
+
+
+part1()
+part2()
