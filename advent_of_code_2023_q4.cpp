f88a3dd402d751ed11b10e64911a69a7e37697e0
sharky564 <sharkesa@gmail.com>
2023-12-13 00:37:15 +1100

    Implemented Problem 4 Part 1 and 2 in Python and C++.

--- advent_of_code_2023_q4.cpp
+++ advent_of_code_2023_q4.cpp
@@ -5,25 +5,21 @@
 #include <vector>
 #include <algorithm>
 
-int main() {
+void part1() {
     std::ifstream file("input.txt");
     std::string line;
     std::vector<std::string> games;
-    // read each line of input into games
     while (std::getline(file, line)) {
         games.push_back(line);
     }
-    std::vector<int> num_cards = std::vector(games.size(), 1);
     int sum = 0;
     int curr_card = 0;
     for (std::string line: games) {
         line = line.substr(line.find(":") + 1);
-        // split along the |, first half is winning numbers, second half is played numbers
         std::string first_half = line.substr(0, line.find("|"));
         std::string second_half = line.substr(line.find("|") + 1);
         std::vector<int> wins = std::vector<int>();
         std::vector<int> plays = std::vector<int>();
-        // split along the spaces
         std::stringstream ss(first_half);
         int value;
         while (ss >> value) {
@@ -33,7 +29,46 @@ int main() {
         while (ss2 >> value) {
             plays.push_back(value);
         }
-        // count number of played numbers inside the wins
+        int j = 0;
+        for (int i = 0; i < plays.size(); i++) {
+            if (std::find(wins.begin(), wins.end(), plays[i]) != wins.end()) {
+                j++;
+            }
+        }
+        if (j > 0) {
+            sum += 1 << (j - 1);
+        }
+        curr_card++;
+    }
+    std::cout << sum << std::endl;
+}
+
+
+void part2() {
+    std::ifstream file("input.txt");
+    std::string line;
+    std::vector<std::string> games;
+    while (std::getline(file, line)) {
+        games.push_back(line);
+    }
+    std::vector<int> num_cards = std::vector(games.size(), 1);
+    int sum = 0;
+    int curr_card = 0;
+    for (std::string line: games) {
+        line = line.substr(line.find(":") + 1);
+        std::string first_half = line.substr(0, line.find("|"));
+        std::string second_half = line.substr(line.find("|") + 1);
+        std::vector<int> wins = std::vector<int>();
+        std::vector<int> plays = std::vector<int>();
+        std::stringstream ss(first_half);
+        int value;
+        while (ss >> value) {
+            wins.push_back(value);
+        }
+        std::stringstream ss2(second_half);
+        while (ss2 >> value) {
+            plays.push_back(value);
+        }
         int j = 0;
         for (int i = 0; i < plays.size(); i++) {
             if (std::find(wins.begin(), wins.end(), plays[i]) != wins.end()) {
@@ -45,5 +80,10 @@ int main() {
         curr_card++;
     }
     std::cout << sum << std::endl;
+}
+
+int main() {
+    part1();
+    part2();
     return 0;
 }
--- advent_of_code_2023_q4.py
+++ advent_of_code_2023_q4.py
@@ -1,17 +1,36 @@
 f = open("input.txt", 'r')
 out = f.read().split('\n')
-total = 0
-num_cards = [1 for _ in range(len(out))]
-for line in out:
-    win, nums = line.split(':')[1].split('|')
-    win = win.split()
-    nums = nums.split()
-    x = num_cards[0]
-    total += x
-    num_cards = num_cards[1:]
-    new_cards = len([x for x in nums if x in win])
-    for i in range(new_cards):
-        num_cards[i] += x
+
+
+def part1():
+    total = 0
+    for line in out:
+        win, nums = line.split(':')[1].split('|')
+        win = win.split()
+        nums = nums.split()
+        num_wins = len([x for x in nums if x in win])
+        if num_wins > 0:
+            total += 1 << (num_wins - 1)
+    
     print(total)
 
-print(total)
+
+def part2():
+    total = 0
+    num_cards = [1 for _ in range(len(out))]
+    for line in out:
+        win, nums = line.split(':')[1].split('|')
+        win = win.split()
+        nums = nums.split()
+        x = num_cards[0]
+        total += x
+        num_cards = num_cards[1:]
+        new_cards = len([x for x in nums if x in win])
+        for i in range(new_cards):
+            num_cards[i] += x
+
+    print(total)
+
+
+part1()
+part2()
